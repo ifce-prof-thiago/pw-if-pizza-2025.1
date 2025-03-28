c092e55be14b6e1afece1668a7187a28856da7af
Thiago Bandeira <thiago@lar.ifce.edu.br>
2025-03-28 10:27:39 -0300

    Conexão com banco de dados

--- src/main/java/ifce/pizza/Main.java
+++ src/main/java/ifce/pizza/Main.java
@@ -1,12 +1,46 @@
 package ifce.pizza;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Configuration
+    public class Init {
+
+        @Autowired
+        private JdbcTemplate jdbcTemplate;
+
+        @Bean
+        public String insert() {
+            var ingredienteId = UUID.randomUUID().toString();
+            var nome = "Marguerita";
+            var disponibilizado = false;
+            var precoP = 10.00;
+            var precoM = 15.00;
+            var precoG = 20.00;
+            jdbcTemplate.update(
+                    "INSERT INTO Ingrediente VALUES (?, ?, ?, ?, ?, ?)",
+                    ingredienteId,
+                    nome,
+                    disponibilizado,
+                    precoP,
+                    precoM,
+                    precoG
+            );
+            return "";
+        }
+
     }
 }
